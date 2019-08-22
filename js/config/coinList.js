94b8b199372a6410b7b894bfbbeb5ccf6b5571cb
uniondao <53466265+uniondao@users.noreply.github.com>
2019-08-23 03:35:18 +0800

    Add files via upload

--- js/config/coinList.js
+++ js/config/coinList.js
@@ -10,39 +10,14 @@ var coins = [
         "shortName":"ETH",
     },
     {
-        "name":"Dai",
-        "logo":"./images/coin/dai.png",
-        "shortName":"DAI",
-    },
-    {
         "name":"wBTC",
         "logo":"./images/coin/wbtc.png",
         "shortName":"WBTC",
     },
     {
-        "name":"Basic Attention Token",
-        "logo":"./images/coin/bat.png",
-        "shortName":"BAT",
-    },
-    {
-        "name":"Maker",
-        "logo":"./images/coin/mkr.png",
-        "shortName":"MKR",
-    },
-    {
-        "name":"TokenCard",
-        "logo":"./images/coin/tkn.png",
-        "shortName":"TKN",
-    },
-    {
-        "name":"TrueUSD",
-        "logo":"./images/coin/tusd.png",
-        "shortName":"TUSD",
-    },
-    {
-        "name":"SpankChain",
-        "logo":"./images/coin/spank.png",
-        "shortName":"SPANK",
+        "name":"Dai",
+        "logo":"./images/coin/dai.png",
+        "shortName":"DAI",
     },
     {
         "name":"USD Coin",
@@ -50,9 +25,44 @@ var coins = [
         "shortName":"USDC",
     },
     {
+        "name":"TrueUSD",
+        "logo":"./images/coin/tusd.png",
+        "shortName":"TUSD",
+    },
+    {
+        "name":"Maker",
+        "logo":"./images/coin/mkr.png",
+        "shortName":"MKR",
+    },
+    {
+        "name":"Sether",
+        "logo":"./images/coin/seth.png",
+        "shortName":"SETH",
+    },
+    {
+        "name":"TokenCard",
+        "logo":"./images/coin/tkn.png",
+        "shortName":"TKN",
+    },
+    {
+        "name":"Basic Attention Token",
+        "logo":"./images/coin/bat.png",
+        "shortName":"BAT",
+    },
+    {
+        "name":"SpankChain",
+        "logo":"./images/coin/spank.png",
+        "shortName":"SPANK",
+    },
+    {
         "name":"SNX",
         "logo":"./images/coin/snx.png",
         "shortName":"SNX",
+    },
+    {
+        "name":"XCHF",
+        "logo":"./images/coin/xchf.png",
+        "shortName":"XCHF",
     },
     {
         "name":"Augur",
@@ -70,19 +80,14 @@ var coins = [
         "shortName":"ANT",
     },
     {
-        "name":"ChainLink",
-        "logo":"./images/coin/link.png",
-        "shortName":"LINK",
-    },
-    {
         "name":"Gnosis",
         "logo":"./images/coin/gno.png",
         "shortName":"GNO",
     },
     {
-        "name":"Loopring",
-        "logo":"./images/coin/lrc.png",
-        "shortName":"LRC",
+        "name":"ChainLink",
+        "logo":"./images/coin/link.png",
+        "shortName":"LINK",
     },
     {
         "name":"0x",
@@ -95,8 +100,287 @@ var coins = [
         "shortName":"FUN",
     },
     {
+        "name":"RPL",
+        "logo":"./images/coin/rpl.png",
+        "shortName":"RPL",
+    },
+    {
+        "name":"SNT",
+        "logo":"./images/coin/snt.png",
+        "shortName":"SNT",
+    },
+    {
+        "name":"NMR",
+        "logo":"./images/coin/nmr.png",
+        "shortName":"NMR",
+    },
+    {
+        "name":"PNK",
+        "logo":"./images/coin/pnk.png",
+        "shortName":"PNK",
+    },
+    {
+        "name":"NEXO",
+        "logo":"./images/coin/nexo.png",
+        "shortName":"NEXO",
+    },
+    {
+        "name":"RLC",
+        "logo":"./images/coin/rlc.png",
+        "shortName":"RLC",
+    },
+    {
+        "name":"HOT",
+        "logo":"./images/coin/hot.png",
+        "shortName":"HOT",
+    },
+    {
+        "name":"KNC",
+        "logo":"./images/coin/knc.png",
+        "shortName":"KNC",
+    },
+    {
+        "name":"BNT",
+        "logo":"./images/coin/bnt.png",
+        "shortName":"BNT",
+    },
+    {
+        "name":"RDN",
+        "logo":"./images/coin/rdn.png",
+        "shortName":"RDN",
+    },
+    {
+        "name":"LOOM",
+        "logo":"./images/coin/loom.png",
+        "shortName":"LOOM",
+    },
+    {
+        "name":"SOCKS",
+        "logo":"./images/coin/socks.png",
+        "shortName":"SOCKS",
+    },
+    {
+        "name":"BLT",
+        "logo":"./images/coin/blt.png",
+        "shortName":"BLT",
+    },
+    {
+        "name":"GRID",
+        "logo":"./images/coin/grid.png",
+        "shortName":"GRID",
+    },
+    {
+        "name":"MANA",
+        "logo":"./images/coin/mana.png",
+        "shortName":"MANA",
+    },
+    {
+        "name":"RCN",
+        "logo":"./images/coin/rcn.png",
+        "shortName":"RCN",
+    },
+    {
+        "name":"Loopring",
+        "logo":"./images/coin/lrc.png",
+        "shortName":"LRC",
+    },
+    {
+        "name":"FOAM",
+        "logo":"./images/coin/foam.png",
+        "shortName":"FOAM",
+    },
+    {
+        "name":"sUSD",
+        "logo":"./images/coin/susd.png",
+        "shortName":"SUSD",
+    },
+    {
         "name":"DAOstack",
         "logo":"./images/coin/gen.png",
         "shortName":"GEN",
     },
+    {
+        "name":"AMPL",
+        "logo":"./images/coin/ampl.png",
+        "shortName":"AMPL",
+    },
+    {
+        "name":"ENJ",
+        "logo":"./images/coin/enj.png",
+        "shortName":"ENJ",
+    },
+    {
+        "name":"FTX",
+        "logo":"./images/coin/ftx.png",
+        "shortName":"FTX",
+    },
+    {
+        "name":"STORJ",
+        "logo":"./images/coin/storj.png",
+        "shortName":"STORJ",
+    },
+    {
+        "name":"C20",
+        "logo":"./images/coin/c20.png",
+        "shortName":"C20",
+    },
+    {
+        "name":"IDRT",
+        "logo":"./images/coin/idrt.png",
+        "shortName":"IDRT",
+    },
+    {
+        "name":"cUSDC",
+        "logo":"./images/coin/cusdc.png",
+        "shortName":"cUSDC",
+    },
+    {
+        "name":"LPT",
+        "logo":"./images/coin/lpt.png",
+        "shortName":"LPT",
+    },
+    {
+        "name":"DATA",
+        "logo":"./images/coin/data.png",
+        "shortName":"DATA",
+    },
+    {
+        "name":"POA20",
+        "logo":"./images/coin/poa20.png",
+        "shortName":"POA20",
+    },
+    {
+        "name":"REN",
+        "logo":"./images/coin/ren.png",
+        "shortName":"REN",
+    },
+    {
+        "name":"REQ",
+        "logo":"./images/coin/req.png",
+        "shortName":"REQ",
+    },
+    {
+        "name":"SWT",
+        "logo":"./images/coin/swt.png",
+        "shortName":"SWT",
+    },
+    {
+        "name":"SALT",
+        "logo":"./images/coin/salt.png",
+        "shortName":"SALT",
+    },
+    {
+        "name":"LQD,",
+        "logo":"./images/coin/lqd.png",
+        "shortName":"LQD,",
+    },
+    {
+        "name":"WCK",
+        "logo":"./images/coin/wck.png",
+        "shortName":"WCK",
+    },
+    {
+        "name":"HUM",
+        "logo":"./images/coin/hum.png",
+        "shortName":"HUM",
+    },
+    {
+        "name":"KIN",
+        "logo":"./images/coin/kin.png",
+        "shortName":"KIN",
+    },
+    {
+        "name":"AMN",
+        "logo":"./images/coin/amn.png",
+        "shortName":"AMN",
+    },
+    {
+        "name":"CVC",
+        "logo":"./images/coin/cvc.png",
+        "shortName":"CVC",
+    },
+    {
+        "name":"CAN",
+        "logo":"./images/coin/can.png",
+        "shortName":"CAN",
+    },
+    {
+        "name":"AST",
+        "logo":"./images/coin/ast.png",
+        "shortName":"AST",
+    },
+    {
+        "name":"MATIC",
+        "logo":"./images/coin/matic.png",
+        "shortName":"MATIC",
+    },
+    {
+        "name":"WMC",
+        "logo":"./images/coin/wmc.png",
+        "shortName":"WMC",
+    },
+    {
+        "name":"ELET",
+        "logo":"./images/coin/elet.png",
+        "shortName":"ELET",
+    },
+    {
+        "name":"LEND",
+        "logo":"./images/coin/lend.png",
+        "shortName":"LEND",
+    },
+    {
+        "name":"PAX",
+        "logo":"./images/coin/pax.png",
+        "shortName":"PAX",
+    },
+    {
+        "name":"ETHPLO",
+        "logo":"./images/coin/ethplo.png",
+        "shortName":"ETHPLO",
+    },
+    {
+        "name":"QCH",
+        "logo":"./images/coin/qch.png",
+        "shortName":"QCH",
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ];
