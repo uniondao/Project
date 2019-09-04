3b054a792bbb4c44b9143e335eed65414869f1bc
uniondao <53466265+uniondao@users.noreply.github.com>
2019-09-04 19:23:08 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,35 +1,20 @@
-const CONFIG = {
+var CONFIG = {
     //Address
-    // "und_addr": "0x7c6c3b4e91923f080d6cc847a68d7330400a95d7",
-    // "udao_addr": "0x4a32e09c60af50f0a5869bf5c622f4e5d57c3b37",
-    // "usdt_addr": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-    // "dai_addr": "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359",
-    // "tusd_addr": "0x0000000000085d4780B73119b644AE5ecd22b376",
-    // "usdc_addr": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-    // "pax_addr": "0x8E870D67F660D95d5be530380D0eC0bd388289E1",
-    // "reserve_addr": "0x425984f014ca5b7ea5ad5f5e27687f6b8da1a02a",
-    //
-    // "und_issue_addr": "0xbb068d536a1151ed1a5ade9b0da8f3252dfd2f81",
-    // "udao_issue_addr": "0xd42cc2d715d0385646149adff1249f5624a74b5c", //  0x426e90e1dcf04c02407a7401e9b74b162e3c6f58
-    // "candy_addr": "0x425984f014ca5b7ea5ad5f5e27687f6b8da1a02a",
-    "und_addr": "0x960Ee18F2aC33209370580D85B9DC6f1A11006cd",  //0x960Ee18F2aC33209370580D85B9DC6f1A11006cd  0x9b33bcb1143367fdc4069c8658543c01c33044e4
-    "udao_addr": "0x23edca64c6fa8d2f35e5338528ce9cacdc3863e4",
-    "usdt_addr": "0xbC5D4D074BAA3431466c8952C750dc6D579d9691",
-    "dai_addr": "0xfd7cd115a188cd92bf42afb9c574999ff5cc3776",
-    "tusd_addr": "0x8e6ffd7c41ee9481105f6a74851857c170cd1515",
-    "usdc_addr": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-    "pax_addr": "0x8e870d67f660d95d5be530380d0ec0bd388289e1",
-    "factory_addr": "0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95",
-    "exchange_addr": "0x32a17ae3d6684105acb39e0c33699fc398a9522e",
+    "und_addr": "0x7c6c3b4e91923f080d6cc847a68d7330400a95d7",  
+    "udao_addr": "0x4a32e09c60af50f0a5869bf5c622f4e5d57c3b37",
+    "usdt_addr": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    "dai_addr": "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359",
+    "tusd_addr": "0x0000000000085d4780B73119b644AE5ecd22b376",
+    "usdc_addr": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    "pax_addr": "0x8E870D67F660D95d5be530380D0eC0bd388289E1",
     "reserve_addr": "0x425984f014ca5b7ea5ad5f5e27687f6b8da1a02a",
 
-    "und_issue_addr": "0x34d0f683d109c645f0047686c527a4e9ef85470b",
-    "udao_issue_addr": "0x9c223905c7d2d432aabbd9470b49c71494a66b70", //  0x426e90e1dcf04c02407a7401e9b74b162e3c6f58
-    "candy_addr": "0x9bdedd179dfe6bc95a36b68a884fa7f297db2d5d",
-    "query_addr" : "0xc9847d92a1429ed331f6bb94e1dbf7d5c26c690c",
-    "airdrop_addr": "0xb4043b6763d9e591ed87359eb8c52bbfa8136778",  //0x66bf17fcc2b9bd4032db93b1412453e3bb069a2a
-    "team_addr" : "0x076fe3a16726ef7e5563b27593c804c267422a4b",
-
+    "und_issue_addr": "0xbb068d536a1151ed1a5ade9b0da8f3252dfd2f81",
+    "udao_issue_addr": "0xd42cc2d715d0385646149adff1249f5624a74b5c", //  0x426e90e1dcf04c02407a7401e9b74b162e3c6f58
+    "candy_addr": "0x425984f014ca5b7ea5ad5f5e27687f6b8da1a02a",
+    "airdrop_addr": "0x8c13262a3B95226D89e3B75C063eB688Ac952f17",  //0x66bf17fcc2b9bd4032db93b1412453e3bb069a2a
+    "team_addr" : "0x3Fec46195D051675af3947e8022f5460a983c145",
+    "investment_addr" : "0xfd459f811c0d61d0a75493d67237d651aaa09e28",
     //Format
     "Fixed": 4,
     "Fixed2": 2,
@@ -44,11 +29,11 @@ const CONFIG = {
     "pax_wei": "ether",
 
     // address2coin
-    "0xbC5D4D074BAA3431466c8952C750dc6D579d9691": "USDT",
-    "0xfD7Cd115a188cD92bF42aFB9c574999ff5cC3776": "DAI",
-    "0x8e6FFd7C41Ee9481105F6a74851857c170Cd1515": "TUSD",
-    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": "USDC",
-    "0x8e870d67f660d95d5be530380d0ec0bd388289e1": "PAX",
+    "0xdAC17F958D2ee523a2206206994597C13D831ec7": "USDT",
+    "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359": "DAI",
+    "0x0000000000085d4780B73119b644AE5ecd22b376": "TUSD",
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "USDC",
+    "0x8E870D67F660D95d5be530380D0eC0bd388289E1": "PAX",
 
     "USDT": "ether",
     "DAI": "ether",
